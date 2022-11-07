424b3aa796790577f8f65aeaa5b0def376332fdc
swipedsalad <89316572+swipedsalad@users.noreply.github.com>
2022-11-07 12:59:52 -0800

    Update thrusterSub.py

--- src/thrusterSub.py
+++ src/thrusterSub.py
@@ -10,8 +10,11 @@ import adafruit_pca9685
 
 def callback(msg):
     rospy.loginfo(msg)
-    dc1 = msg.t1
-    dc2 = msg.t2
+    
+    dc1 = int(((((msg.t1 * 3) + 1500) / 10_000) * 65536))
+    dc2 = (msg.t2 * 3)
+    
+    
     rospy.loginfo(dc1)
     rospy.loginfo(dc2)
     
@@ -26,13 +29,30 @@ if __name__ == '__main__':
     shield = adafruit_pca9685.PCA9685(i2c)
     kit = ServoKit(channels = 16)
     shield.frequency = 100
+    
     thruster_channel0 = shield.channels[0]
     thruster_channel1 = shield.channels[1]
+    thruster_channel2 = shield.channels[2]
+    thruster_channel3 = shield.channels[3]
+    thruster_channel4 = shield.channels[4]
+    thruster_channel5 = shield.channels[5]
 
     thruster_channel0.duty_cycle = 0x2666
     print("Done")
     time.sleep(2)
     thruster_channel1.duty_cycle = 0x2666
+    print("Done")
+    time.sleep(2)
+    thruster_channel2.duty_cycle = 0x2666
+    print("Done")
+    time.sleep(2)
+    thruster_channel3.duty_cycle = 0x2666
+    print("Done")
+    time.sleep(2)
+    thruster_channel4.duty_cycle = 0x2666
+    print("Done")
+    time.sleep(2)
+    thruster_channel5.duty_cycle = 0x2666
     print("Done")
     time.sleep(2)
 
