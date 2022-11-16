d871589b4a8d0ecde54f2b6518bab67361c37ed2
James Randall <84990498+James-Randall-14@users.noreply.github.com>
2022-11-16 09:09:06 -0800

    Update thrusterInterface.py

--- src/thrusterInterface.py
+++ src/thrusterInterface.py
@@ -8,7 +8,8 @@ import board
 import busio
 import adafruit_pca9685
 
-def thrusterCallback(msg, thruster_channels):
+def thrusterCallback(msg):
+    global thruster_channels
     rospy.loginfo(msg)
     
     msglist = [msg.t1, msg.t2, msg.t3, msg.t4, msg.t5, msg.t6]
@@ -16,7 +17,7 @@ def thrusterCallback(msg, thruster_channels):
     rospy.loginfo(msglist)
     
     for i in range(6);
-        thruster_channels[i].duty_cycle = int(((msglist[i] * (2.0/5)+ 1500)*6.5536))
+        thruster_channels[i].duty_cycle = int(((msglist[i] * 0.4 + 1500) * 6.5536))
         
     rospy.loginfo(thruster_channels)
 
@@ -33,9 +34,9 @@ if __name__ == '__main__':
         print("Done")
         time.sleep(0.1)
 
-    rospy.init_node('thrusterSub', anonymous=True)
+    rospy.init_node('thrusterInterface')
 
-    rospy.Subscriber("thusters", thrusterPercents, callback = thrusterCallback(msg, thruster_channels))
+    rospy.Subscriber("thusters", thrusterPercents, thrusterCallback)
 
     
 
