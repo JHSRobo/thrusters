fe9ae91e29ae00f7daa9c06d0ad679f86c180871
James-Randall-14 <jammerand14@gmail.com>
2023-01-22 12:36:44 -0800

    minor syntax change sorry for so many pings in discord lmfao (skill issue)

--- src/thrusterInterface.py
+++ src/thrusterInterface.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     rospy.init_node('thruster_interface')
     i2c = busio.I2C(board.SCL, board.SDA)
     try: shield = adafruit_pca9685.PCA9685(i2c)
-    except: rospy.logwarn("Can't connect to PCA9685. Ignore this if thrusters are unplugged.")
+    except: rospy.logwarn("Cannot connect to PCA9685. Ignore this if thrusters are unplugged.")
     else: 
         kit = ServoKit(channels = 16)
         shield.frequency = 100
