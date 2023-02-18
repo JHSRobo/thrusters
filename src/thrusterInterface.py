eef002764165e32fdd25f95a59a09e907fbdd856
swipedsalad <PetersonN24@student.jhs.net>
2023-02-17 22:26:40 -0800

    minor tweaks, still broken

--- src/thrusterInterface.py
+++ src/thrusterInterface.py
@@ -8,10 +8,11 @@ from pca9685 import PCA9685
 def thrusterCallback(msg):
     global thruster_channels
     msglist = [msg.t1, msg.t2, msg.t3, msg.t4, msg.t5, msg.t6]
-    
+    duty_cycle = [0, 0, 0, 0, 0, 0]
     for i in range(6):
-        duty_cycle = int(msglist[i] * 0.163 + 614)
-        pca.channel_set_duty(i, duty_cycle)
+        duty_cycle[i] = int(msglist[i] * 0.4 + 1100)
+        pca.channel_set_duty(i, duty_cycle[i])
+    rospy.logwarn(duty_cycle)
 
 if __name__ == '__main__':
     rospy.init_node('thruster_interface')
@@ -22,9 +23,9 @@ if __name__ == '__main__':
     pca.output_enable()
 
     # Initialize the thrusters
-    pca.channels_set_duty_all(614)
+    pca.channels_set_duty_all(1500)
     time.sleep(0.1)
-        
+
     rospy.Subscriber("thrusters", thrusterPercents, thrusterCallback)
 
     rospy.spin()
